9bd91ddbcc1c199eb2c6510d8deb5e7160261f90
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-19 14:51:40 +0600

    header is more good now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,80 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import headerbg from '../assets/images/more/headerbg.jpg';
+import logo from '../assets/images/more/logo.png';
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const links = (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          `mr-3 transition-all duration-200 hover:scale-95 ${
+            isActive
+              ? 'text-[#E3B577] font-bold scale-110'
+              : 'text-white text-base'
+          }`
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) =>
+          `mr-3 transition-all duration-200 hover:scale-95 ${
+            isActive
+              ? 'text-[#E3B577] font-bold scale-110'
+              : 'text-white text-base'
+          }`
+        }
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/Coffes"
+        className={({ isActive }) =>
+          `mr-3 transition-all duration-200 hover:scale-95 ${
+            isActive
+              ? 'text-[#E3B577] font-bold scale-110'
+              : 'text-white text-base'
+          }`
+        }
+      >
+        Coffes
+      </NavLink>
+      <NavLink
+        to="/contact"
+        className={({ isActive }) =>
+          `mr-3 transition-all duration-200 hover:scale-95 ${
+            isActive
+              ? 'text-[#E3B577] font-bold scale-110'
+              : 'text-white text-base'
+          }`
+        }
+      >
+        Contact
+      </NavLink>
+    </>
+  );
+
   return (
-    <header className="flex items-center justify-between px-6 py-3 md:py-4   mx-auto w-full bg-white">
+    <header
+      className="flex items-center justify-between px-6 py-3 md:py-4 mx-auto w-full bg-cover bg-center"
+      style={{ backgroundImage: `url(${headerbg})` }}
+    >
       {/* Logo */}
-     <Link className='text-2xl font-bold text-indigo-700'>Coffe Store</Link>
+      <Link className="text-2xl font-bold text-white">
+        <img src={logo} className="max-w-[250px]" alt="Coffe Logo" />
+      </Link>
 
       {/* Navigation Menu */}
       <nav
-        className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:overflow-hidden items-center justify-center max-md:h-full transition-[width] bg-white/50 backdrop-blur flex-col md:flex-row flex gap-8 text-gray-900 text-sm font-normal
+        className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:overflow-hidden items-center justify-center max-md:h-full transition-[width]  backdrop-blur flex-col md:flex-row flex gap-8 text-gray-900 text-sm font-normal
           ${menuOpen ? 'max-md:w-full' : 'max-md:w-0'}`}
       >
-        <a className="hover:text-indigo-600" href="#">
-          Products
-        </a>
-        <a className="hover:text-indigo-600" href="#">
-          Customer Stories
-        </a>
-        <a className="hover:text-indigo-600" href="#">
-          Pricing
-        </a>
-        <a className="hover:text-indigo-600" href="#">
-          Docs
-        </a>
+        {links}
 
         {/* Close Menu Button */}
         <button
@@ -48,12 +97,13 @@ export default function Header() {
 
       {/* Right-side Buttons */}
       <div className="flex items-center space-x-4">
-        <a
-          className="hidden md:flex bg-indigo-600 text-white px-5 py-2 rounded-full text-sm font-medium hover:bg-indigo-700 transition"
+        <Link
+          to="/"
+          className="hidden md:flex bg-[#E3B577] text-black px-5 py-2 rounded-full text-sm font-medium hover:border-2 hover:border-white hover:bg-transparent hover:text-white transition"
           href="#"
         >
           Sign up
-        </a>
+        </Link>
 
         {/* Open Menu Button */}
         <button
