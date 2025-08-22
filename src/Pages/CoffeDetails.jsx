60ca6013bd62bb1766b35d6b560b36d0fb95eb63
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-22 10:56:54 +0600

    now when click view icon from home page it's showing the exact single coffe information

--- src/Pages/CoffeDetails.jsx
+++ src/Pages/CoffeDetails.jsx
@@ -1,13 +1,61 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import bgImg from '../assets/images/more/bg2.png';
 
 const CoffeDetails = () => {
   const coffe = useLoaderData();
-  const { _id, name, quantity, supplier, taste, category, description, photoUrl } = coffe;
+  const {
+    _id,
+    name,
+    quantity,
+    supplier,
+    taste,
+    category,
+    description,
+    photoUrl,
+  } = coffe;
 
   return (
-    <div>
-      <h2>Coffe Details {name}</h2>
+    <div
+      className="bg-no-repeat bg-cover bg-center  p-22"
+      style={{ backgroundImage: `url(${bgImg})` }}
+    >
+      <div className="w-3/4 mx-auto bg-[#F3F3F0] rounded border border-[#382827] py-10">
+        {/* Card */}
+
+        <div className="relative flex items-center overflow-hidden">
+          <img className="w-[200px]" src={photoUrl} alt="Blog Image" />
+
+          <div className="grow p-4 ms-32 sm:ms-48">
+            <div className="min-h-24 flex flex-col justify-center">
+              <h3 className="font-semibold text-xl text-[#382827]">
+                Name: <span className=" text-lg font-normal">{name}</span>
+              </h3>
+              <p className="mt-1 text-base font-medium text-[#382827]">
+                Available Quantity:{' '}
+                <span className="text-sm font-normal">{quantity}</span>
+              </p>
+              <p className="mt-1 text-base font-medium text-[#382827]">
+                Taste: <span className="text-sm font-normal">{taste}</span>
+              </p>
+              <p className="mt-1 text-base font-medium text-[#382827]">
+                Supplier:{' '}
+                <span className="text-sm font-normal">{supplier}</span>
+              </p>
+              <p className="mt-1 text-base font-medium text-[#382827]">
+                Category:{' '}
+                <span className="text-sm font-normal">{category}</span>
+              </p>
+              <p className="mt-1 text-base font-medium text-[#382827]">
+                Description:{' '}
+                <span className="text-sm font-normal">{description}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* End Card */}
+      </div>
     </div>
   );
 };
