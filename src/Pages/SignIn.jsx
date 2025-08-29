05e23e88533e9cb18b9c494fe1a3f25a53ffd91f
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-29 22:12:24 +0600

    signInUser function created into the provider and it has 2peramiter and using firebase hook called signInWithEmailAndPassword(auth, email, password) and then called the context from signIn.jsx and user are able to logged in

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const SignIn = () => {
+
+  const {signInUser} = useContext(AuthContext);
+
   const handleSignIn = (e) => {
     e.preventDefault();
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password);
+
+    signInUser(email, password)
+    .then(result => {
+      console.log(result.user);
+
+    })
+    .catch(error => {
+      console.log(error.message);
+    })
   };
   return (
     <div className="flex justify-center py-10">
